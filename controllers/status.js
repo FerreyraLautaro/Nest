e8a952510880424f264dbdbe9e17bdddef010547
Apkahym <apkahym@hotmail.com>
2020-10-27 22:06:35 -0300

    feat(Module): add status controller and routes for check code, node engine and database status

---
+++ controllers/status.js
@@ -0,0 +1,22 @@
+/**
+ * Requiering dependencies
+ */
+const { knex, error } = require('_config');
+const { toString, assign } = require('lodash');
+
+/**
+ * @function DbVersion
+ * @description This function get version of MySQL/MariaDB database engine
+ */
+const DbVersion = () => {
+  try {
+    return knex.raw('select version() as version');
+  } catch (err) {
+    error(toString(err));
+    return toString(err);
+  }
+};
+
+module.exports = {
+  DbVersion,
+};
